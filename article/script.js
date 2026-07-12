863e20aa8838f5ce21d99870267cbb195755bbde
GeloRay <gusg6330@gmail.com>
2026-07-12 12:10:47 +0800

    fix article & index; fix script.js&style.css

--- article/script.js
+++ article/script.js
@@ -2,7 +2,8 @@
 let imgs = document.querySelectorAll("img");
 imgs.forEach((v, k, p)=>{
     if (v.alt != null && v.alt != undefined){
-        let introduction = document.createElement("section")
+        let introduction = document.createElement("section");
+        introduction.textContent = v.alt;
         v.parentNode.insertBefore(introduction, v.nextSibling);
     }
 });
